f5e34c115c60c716bb9056e8311779e2e811b2f2
anjana-radhakrishnan <111497171+anjana-radhakrishnan@users.noreply.github.com>
2025-01-28 14:49:34 +0530

    Updated the backend.tf with path to statefile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "fluxcd-bucket-poc"    
-    key            = "poc" # Update it
+    key            = "poc/terraform.tfstate" 
     region         = "us-east-1"                          
     encrypt        = true
   }
