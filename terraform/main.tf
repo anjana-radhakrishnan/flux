23d06991cff520dc5198f53403bc562b85cb5f9e
anjana-radhakrishnan <111497171+anjana-radhakrishnan@users.noreply.github.com>
2025-01-28 10:33:06 +0530

    Updated the main.tf file by enabling the cluster endpoint public access as true

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,10 +26,10 @@ module "vpc" {
   version = "~> 19.0"
   cluster_name    = "flux-cluster" #EKS cluster name
   cluster_version = "1.24" #EKS cluster version
-  cluster_endpoint_private_access = true #Enabling cluster endpoint private access
-  cluster_endpoint_public_access  = false  #disabling endpoint public access
+  cluster_endpoint_private_access = false #Enabling cluster endpoint private access
+  cluster_endpoint_public_access  = true  #disabling endpoint public access
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
   create_iam_role = true #Creating cluster role
   iam_role_name = "flux-eks-role" #name for cluster role
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
