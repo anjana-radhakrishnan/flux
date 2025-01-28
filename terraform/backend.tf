d5c46748b761b2df22d36a6b48ab801d91ef462c
anjana-radhakrishnan <111497171+anjana-radhakrishnan@users.noreply.github.com>
2025-01-28 14:35:33 +0530

    Updated the backend.tf with closing bracket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fluxcd-bucket-poc"     # Update it 
+    bucket         = "fluxcd-bucket-poc"    
     key            = "poc" # Update it
-    region         = "us-east-1"                            # Update it
+    region         = "us-east-1"                          
     encrypt        = true
   }
+}
 
