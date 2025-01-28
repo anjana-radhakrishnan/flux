f3b62a1760c499548ffd80cce8772e15df3af088
anjana-radhakrishnan <111497171+anjana-radhakrishnan@users.noreply.github.com>
2025-01-28 11:24:40 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = fluxcd-bucket-poc       # Replace with your S3 bucket name
-    region         = us-east-1              # Replace with your AWS region
+    bucket         = "fluxcd-bucket-poc"       # Replace with your S3 bucket name
+    region         = "us-east-1"              # Replace with your AWS region
     encrypt        = true
   }
 }
